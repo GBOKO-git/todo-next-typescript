e3e8d7a837c9d47123750c3b1bea594b3d6ca8b3
GBOKO-git <gboko.amara@uvci.edu.ci>
2025-05-09 02:40:19 +0000

    feat: logique et affichage terminé

--- src/app/task/page.tsx
+++ src/app/task/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from "next/link";
-import { getTodos, toggleTodo, deleteTodo } from "@/Geteways/todo";
+import { getTodos, toggleTodo } from "@/Geteways/todo";
 import { useEffect, useState } from "react";
 import { ITodo } from "@/Interfaces/todo";
 
@@ -11,17 +11,17 @@ const Task = () => {
     useEffect(() => {
         setTodos(getTodos());
     }, []);
-  
-    
+
+
     const handleToggleTodo = (id: number) => {
         toggleTodo(id);
         setTodos((prevTodos) =>
-          prevTodos.map((todo) =>
-            todo.id === id ? { ...todo, completed: !todo.completed } : todo
-          )
+            prevTodos.map((todo) =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
         );
-      };
-    
+    };
+
 
     return (
         <>
@@ -46,16 +46,16 @@ const Task = () => {
                     {todos.map((todo) => (
                         <li key={todo.id} className="py-4">
                             <div className="flex items-center">
-                                <input checked={todo.completed} id={`todo-${todo.id}`} name={`todo-${todo.id}`} type="checkbox" onChange={() =>handleToggleTodo(todo.id)}
+                                <input checked={todo.completed} id={`todo-${todo.id}`} name={`todo-${todo.id}`} type="checkbox" onChange={() => handleToggleTodo(todo.id)}
                                     className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded" />
                                 <label htmlFor="todo1" className="ml-3 flex justify-between text-gray-900  w-full">
-                                    <div className={todo.completed ?  "text-gray-400 line-through"  : "text-lg font-medium"}>{todo.title}</div>
+                                    <div className={todo.completed ? "text-gray-400 line-through" : "text-lg font-medium"}>{todo.title}</div>
                                     <div className="flex gap-2">
-                                        <Link  href={`/updateTask/${todo.id}`}  >
-                                        <button className="text-sm font-light bg-violet-800 border-1 cursor-pointer hover:bg-violet-950 rounded px-2 text-white">Editer</button>
+                                        <Link href={`/updateTask/${todo.id}`}  >
+                                            <button className="text-sm font-light bg-violet-800 border-1 cursor-pointer hover:bg-violet-950 rounded px-2 text-white">Editer</button>
                                         </Link>
                                         <Link href={`/deleteTask/${todo.id}`} >
-                                        <button  className="text-sm font-light text-slate-100 bg-red-500 cursor-pointer hover:bg-red-950 rounded px-1">Supprimer</button>
+                                            <button className="text-sm font-light text-slate-100 bg-red-500 cursor-pointer hover:bg-red-950 rounded px-1">Supprimer</button>
                                         </Link>
                                     </div>
                                 </label>

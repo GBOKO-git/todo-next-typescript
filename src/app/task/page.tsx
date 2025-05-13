ae56b1c76c1831337e8bbe1f96f24c35824f93b9
GBOKO-git <gboko.amara@uvci.edu.ci>
2025-05-13 23:19:52 +0000

    fix: projet final

--- src/app/task/page.tsx
+++ src/app/task/page.tsx
@@ -54,7 +54,7 @@ const Task = () => {
                                 href="/createTask"
                                 className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
                                 type="button">
-                                Ajouter
+                                + Ajouter
                             </Link>
                         </div>
                     </form>

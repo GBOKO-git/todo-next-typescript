650fe3a85f79cc41d6c6af5c6be253b15bacc3bf
GBOKO-git <gboko.amara@uvci.edu.ci>
2025-05-17 10:04:07 +0000

    fix: remove SVGProps<SVGSVGElement> from deleteTasks

--- src/app/deleteTask/[id]/page.tsx
+++ src/app/deleteTask/[id]/page.tsx
@@ -5,6 +5,7 @@ import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { getTodoById } from "@/Geteways/todo"
 import { ITodo } from "@/Interfaces/todo";
+import { CiSquareRemove } from "react-icons/ci";
 
 const DeleteTask = () => {
   const { id } = useParams();
@@ -46,19 +47,7 @@ const DeleteTask = () => {
             className="absolute top-2 right-2"
             onClick={() => router.back()}
           >
-            <svg
-              title="Close"
-              className="h-4 w-4 text-gray-400"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+            <CiSquareRemove />
             <span className="sr-only">Close</span>
           </button>
 
